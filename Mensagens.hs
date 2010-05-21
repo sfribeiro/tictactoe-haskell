8e8b91cb40d15560ec89eafb157c4dab3e37804e
andersonoliveiramarques <andersonoliveiramarques@5c1608fb-56d2-20c3-ac56-ddccd30acc0e>
2010-05-21 18:24:06 +0000

    Implementação de algumas partes do menu.

--- Mensagens.hs
+++ Mensagens.hs
@@ -26,6 +26,66 @@ menuSair = "Sair"
 ajudaSair :: String
 ajudaSair = "Sai do Jogo da Velha"
 
+menuOpcoes :: String
+menuOpcoes = "&Opcoes"
+
+menuAvisar :: String
+menuAvisar = "Avisar jogada invalida"
+ajudaAvisar :: String
+ajudaAvisar = "Exibicao de uma mensagem quando a jogada for invalida"
+
+menuSkins :: String
+menuSkins = "Selecionar skin"
+
+menuSkin1 :: String
+menuSkin1 = "1. Padrao"
+
+menuSkin2 :: String
+menuSkin2 = "2. LaVermelha"
+
+menuAjuda :: String
+menuAjuda = "&Ajuda"
+
+menuRegras :: String
+menuRegras = "Regras do Jogo"
+ajudaRegras :: String
+ajudaRegras = "Mostra as regras do jogo"
+
+menuSobre :: String
+menuSobre = "Sobre..."
+ajudaSobre :: String
+ajudaSobre = "Sobre o Jogo da Velha"
+
+-- Regras do Jogo
+msgRegrasTitulo :: String
+msgRegrasTitulo = "Regras do Jogo"
+
+msgRegras :: String
+msgRegras =
+    "REGRAS DO JOGO\n" ++
+    "------------------------------------------------------------\n\n" ++
+    "O tabuleiro  e uma matriz  de tres linhas por tres colunas.\n" ++
+    "Dois jogadores escolhem uma marcacao cada um, geralmente um circulo (O) e um xis (X).\n" ++
+    "Os jogadores jogam alternadamente, uma marcacao por vez, numa lacuna que esteja vazia.\n" ++
+    "O objetivo e conseguir tres circulos ou tres xis em linha, quer horizontal, vertical ou diagonal,"++ 
+	"e ao mesmo tempo, quando possivel, impedir o adversario de ganhar na proxima jogada.\n" ++
+    "Quando um jogador conquista o objetivo, costuma-se riscar os tres simbolos."
+
+
+
+msgSobreTitulo :: String
+msgSobreTitulo = "Sobre o Jogo da Velha"
+
+msgSobre :: String
+msgSobre =
+    "Jogo da Velha 1.0\n" ++
+    "------------------------------------------------------------\n\n" ++
+    "Linguagem utilizada: Haskell\n\n" ++ 
+    "Equipe:\n" ++
+    "- Anderson de Oliveira Marques\n" ++
+    "- Rodrigo Cesar Lira da Silva\n" ++
+    "- Sergio Ferreira Ribeiro"
+
 strX:: String
 strX = "X"
 
--- TicTacToe.hs
+++ TicTacToe.hs
@@ -34,12 +34,29 @@ gui = do
 	p_3_2		<- panel p1 [clientSize := sz 128 128]
 	p_3_3		<- panel p1 [clientSize := sz 128 128]
 	
+	--MENU
+	
+	--Jogo
 	mJogo		<- menuPane [text := menuJogo]
 	mNovo1		<- menuItem mJogo [text := menuNovoJogoCPU, help := ajudaNovoJogoCPU]
 	mNovo2		<- menuItem mJogo [text := menuNovoJogo2P, help := ajudaNovoJogo2P]
 	mFecha		<- menuItem mJogo [text := menuFecha, help := ajudaFecha]
 	mJogoL		<- menuLine mJogo --separador
-	mSair 		<- menuQuit mJogo [text := menuSair, help := ajudaSair, on command:= close f]
+	mSair 		<- menuQuit mJogo [text := menuSair, help := ajudaSair]
+	
+	--Opções
+	mOpcoes		<- menuPane [text := menuOpcoes]
+	mAvisar		<- menuItem mOpcoes [text := menuAvisar, help := ajudaAvisar]
+	mJogoL		<- menuLine mJogo --separador
+	mSkins		<- menuPane [text := menuSkins]
+	mSkinsSub	<- menuSub mOpcoes mSkins []
+	mSkin1		<- menuRadioItem mSkins [text := menuSkin1]
+	mSkin2		<- menuRadioItem mSkins [text := menuSkin2]
+	
+	--Ajuda
+	mAjuda		<- menuHelp [text := menuAjuda]
+	mRegras		<- menuItem mAjuda [text := menuRegras, help := ajudaRegras]
+	mSobre		<- menuAbout mAjuda [text := menuSobre, help := ajudaSobre]
 	
 	status		<- statusField [text := ""]
 	
@@ -59,8 +76,22 @@ gui = do
 						
 	amb 		<- toIO (f, tabuleiro, modo, vez, aviso, skin, posicoes, p3, p1, p2, mFecha)
 	
+	--PROPRIEDADES DOS ELEMENTOS
+	
+	--Jogo
+	set mSair [on command := close f]
+	
+	--Opções
+	set mSkin1 [on command := aplicaSkin amb "padrao", checked := True]
+	set mSkin2 [on command := aplicaSkin amb "LaVermelha"]
+	
+	--Ajuda
+	set mRegras [on command := infoDialog f msgRegrasTitulo msgRegras]
+	set mSobre [on command := infoDialog f msgSobreTitulo msgSobre]
+	
+	
 	set f [statusBar := [status],
-		menuBar := [mJogo],
+		menuBar := [mJogo, mOpcoes, mAjuda],
 		layout := column 0 [row 0 [widget p3], row 1 [widget p1], row 2 [widget p2]],
 		clientSize := sz 384 460]
 
