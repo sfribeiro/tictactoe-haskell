501eade498e0e0fc2fa6acb6842c185717530ee6
rodrigocliras <rodrigocliras@5c1608fb-56d2-20c3-ac56-ddccd30acc0e>
2010-05-23 02:12:28 +0000

    Tag da primeira versao

---
+++ tags/release-1.0/TicTacToe.hs
@@ -0,0 +1,141 @@
+module Main where
+
+import Graphics.UI.WX
+import Graphics.UI.WXCore
+import Mensagens
+import Tipos
+import Regras
+import Auxiliar
+
+main :: IO()
+main = start gui
+
+gui :: IO()
+gui = do
+	
+	-- Cria a janela do jogo
+	f 			<- frameFixed [text := tituloJanela, picture := "tictactoe.ico"]
+	
+	-- Cria os paineis da janela
+	
+	p3			<- panel f [clientSize := sz 384 38]
+	p1 			<- panel f [clientSize := sz 384 384]
+	p2			<- panel f [clientSize := sz 384 38]
+	
+	-- Um painel para casa posição no tabuleiro
+	p_1_1		<- panel p1 [clientSize := sz 128 128]
+	p_1_2		<- panel p1 [clientSize := sz 128 128]
+	p_1_3		<- panel p1 [clientSize := sz 128 128]
+	
+	p_2_1		<- panel p1 [clientSize := sz 128 128]
+	p_2_2		<- panel p1 [clientSize := sz 128 128]
+	p_2_3		<- panel p1 [clientSize := sz 128 128]
+	
+	p_3_1		<- panel p1 [clientSize := sz 128 128]
+	p_3_2		<- panel p1 [clientSize := sz 128 128]
+	p_3_3		<- panel p1 [clientSize := sz 128 128]
+	
+	-- MENU
+	
+	-- Jogo
+	mJogo		<- menuPane [text := menuJogo]
+	mNovo1		<- menuPane [text := menuNovoJogoCPU]
+	mNovo1Sub	<- menuSub mJogo mNovo1[]
+	mNovo11		<- menuItem mNovo1 [text := "X", help := ajudaNovoJogoCPU]
+	mNovo12		<- menuItem mNovo1 [text := "O", help := ajudaNovoJogoCPU]
+	mNovo2		<- menuPane [text := menuNovoJogo2P]
+	mNovo2Sub	<- menuSub mJogo mNovo2 []
+	mNovo21		<- menuItem mNovo2 [text := "X", help := ajudaNovoJogo2P]
+	mNovo22		<- menuItem mNovo2 [text := "O", help := ajudaNovoJogo2P]
+	mFecha		<- menuItem mJogo [text := menuFecha, help := ajudaFecha]
+	mJogoL		<- menuLine mJogo --separador
+	mSair 		<- menuQuit mJogo [text := menuSair, help := ajudaSair]
+	
+	-- Opções
+	mOpcoes		<- menuPane [text := menuOpcoes]
+	mAvisar		<- menuItem mOpcoes [text := menuAvisar, help := ajudaAvisar]
+	mSons		<- menuItem mOpcoes [text := menuSons, help := ajudaSons]
+	mJogoL		<- menuLine mJogo --separador
+	mSkins		<- menuPane [text := menuSkins]
+	mSkinsSub	<- menuSub mOpcoes mSkins []
+	mSkin1		<- menuRadioItem mSkins [text := menuSkin1]
+	mSkin2		<- menuRadioItem mSkins [text := menuSkin2]
+	mSkin3      <- menuRadioItem mSkins [text := menuSkin3]
+        mResultados <- menuItem mOpcoes [text := menuResultado, help := ajudaResultado]
+
+	-- Ajuda
+	mAjuda		<- menuHelp [text := menuAjuda]
+	mRegras		<- menuItem mAjuda [text := menuRegras, help := ajudaRegras]
+	mSobre		<- menuAbout mAjuda [text := menuSobre, help := ajudaSobre]
+	
+	-- Barra de status
+	status		<- statusField [text := ""]
+	
+	-- Variáveis do jogo
+	
+	-- Nenhum = 0
+	-- Contra CPU = 1
+	-- 2 jogadores = 2
+	modo		<- variable [value := 0]
+	
+	-- Liga e desliga o aviso de jogada inválida
+	aviso		<- variable [value := True]
+	
+	-- Armazena o tabuleiro atual do jogo
+	tabuleiro	<- variable [value := tabZerado]
+	
+	-- Armazena a vez de jogar
+	vez			<- variable [value := Vazio]
+	
+	-- Armazena a skin utilizada no momento
+	skin		<- variable [value := ""]
+	
+	-- Lista de posições para ser utilizada por outras funções
+	posicoes	<- toIO [p_1_1,p_1_2,p_1_3,
+						p_2_1,p_2_2,p_2_3,
+						p_3_1,p_3_2,p_3_3]
+						
+	-- Ativa ou desativa os sons do jogo
+	sons		<- variable [value := True]
+	
+	-- Cria a estrutura Ambiente com variáveis e elementos do jogo
+	amb 		<- toIO (f, tabuleiro, modo, vez, aviso, skin, posicoes, p3, p1, p2, mFecha, sons)
+	
+	--Propriedades do Elementos
+	
+	-- Menu Jogo
+	set mSair [on command := close f]
+	set mNovo11 [on command := novoJogoP amb 1 X]
+	set mNovo12 [on command := novoJogoP amb 1 O]
+	set mNovo21 [on command := novoJogoP amb 2 X]
+	set mNovo22 [on command := novoJogoP amb 2 O]
+	set mFecha [on command := fecharJogoP amb, enabled := False]
+	
+	-- Menu Opções
+	set mAvisar [on command := mudaAviso aviso mAvisar, checkable := True, checked := True]
+	set mSons [on command := mudaSom sons mSons, checkable := True, checked := True]
+	set mSkin1 [on command := aplicaSkin amb "padrao", checked := True]
+	set mSkin2 [on command := aplicaSkin amb "LaVermelha"]
+	set mSkin3 [on command := aplicaSkin amb "pacman"]
+        set mResultados [on command := infoDialog f msgResultadoTitulo msgResultado ]
+	
+	-- Menu Ajuda
+	set mRegras [on command := infoDialog f msgRegrasTitulo msgRegras]
+	set mSobre [on command := infoDialog f msgSobreTitulo msgSobre]
+	
+	-- Define a disposição dos elementos na janela
+	set f [statusBar := [status],
+		menuBar := [mJogo, mOpcoes, mAjuda],
+		layout := column 0 [row 0 [widget p3], row 1 [widget p1], row 2 [widget p2]],
+		clientSize := sz 384 460]
+
+	-- Define a disposição dos elementos no tabuleiro
+	set p1 [layout := floatCentre $ column 0 [
+			row 1 [row 0 [widget p_1_1, widget p_1_2, widget p_1_3]],
+			row 2 [row 0 [widget p_2_1, widget p_2_2, widget p_2_3]],
+			row 3 [row 0 [widget p_3_1, widget p_3_2, widget p_3_3]]],
+			clientSize := sz 384 384]
+	
+	-- Aplica a skin Padrão
+	aplicaSkin amb "padrao"
+	
---
+++ tags/release-1.0/Tipos.hs
@@ -0,0 +1,118 @@
+module Tipos (
+	Estado (
+		X,
+		O,
+		Vazio
+		),
+	Jogada,
+    Tabuleiro,
+    Ambiente,
+    oposto,
+    ambFrm, ambTbl,
+    ambMod, ambVez,
+    ambAvs, ambSkn,
+    ambPos, ambPn1,
+    ambPn2, ambPn3,
+	ambFch, ambSom, strEstado,
+    toIO
+    ) where
+	
+import Graphics.UI.WX
+import Mensagens
+
+-- Definições de tipos e dados
+
+-- Possível estado de uma posição no tabuleiro
+data Estado = X | O | Vazio deriving (Eq)
+
+-- Tupla com a coordenada da jogada e o estado
+type Jogada = (Int,Int,Estado)
+
+-- Tabuleiro do jogo (lista das jogadas)
+type Tabuleiro = [Jogada]
+
+-- Tupla das variáveis e elementos do jogo
+type Ambiente = (
+    Frame (),       -- Janela principal do jogo
+    Var Tabuleiro,  -- Tabuleiro do jogo
+    Var Int,        -- Modo de jogo
+    Var Estado,     -- Estado que está na vez de jogar
+    Var Bool,       -- Variável de aviso de jogada inválida
+    Var String,     -- Skin do jogo
+    [Panel ()],     -- Lista de posições (painéis) do tabuleiro
+    Panel (),       -- Painel que represente o fundo da janela
+    Panel (),       -- Painel que representa o placar do jogo
+	Panel (),		-- Painel que representa o título do jogo
+    MenuItem (),     -- Item do menu que encerra a partida
+	Var Bool		-- Variável de aviso de ativação do som
+    )
+	
+-- Funções para manipulação dos tipos
+	
+-- Recebe um estado e retorna o seu oposto
+oposto :: Estado -> Estado
+oposto e
+    | e == X  = O
+    | e == O = X
+    | otherwise   = Vazio
+	
+-- Retorna somenta o elemento janela (frame)
+ambFrm :: Ambiente -> Frame ()
+ambFrm (a, _, _, _, _, _, _, _, _, _, _, _) = a
+
+-- Retorna somente o tabuleiro
+ambTbl :: Ambiente -> Var Tabuleiro
+ambTbl (_, a, _, _, _, _, _, _, _, _, _, _) = a
+
+-- Retorna somente o modo de jogo
+ambMod :: Ambiente -> Var Int
+ambMod (_, _, a, _, _, _, _, _, _, _, _, _) = a
+
+-- Retorna somente a variável de vez
+ambVez :: Ambiente -> Var Estado
+ambVez (_, _, _, a, _, _, _, _, _, _, _, _) = a
+
+-- Retorna somente a variável de aviso de jogada inválida
+ambAvs :: Ambiente -> Var Bool
+ambAvs (_, _, _, _, a, _, _, _, _, _, _, _) = a
+
+-- Retorna somente o skin
+ambSkn :: Ambiente -> Var String
+ambSkn (_, _, _, _, _, a, _, _, _, _, _, _) = a
+
+-- Retorna somente a lista de posições (painéis)
+ambPos :: Ambiente -> [Panel ()]
+ambPos (_, _, _, _, _, _, a, _, _, _, _, _) = a
+
+-- Retorna somente o painel do título
+ambPn3 :: Ambiente -> Panel ()
+ambPn3 (_, _, _, _, _, _, _, a, _, _, _, _) = a
+
+-- Retorna somente o painel de fundo
+ambPn1 :: Ambiente -> Panel ()
+ambPn1 (_, _, _, _, _, _, _, _, a, _, _, _) = a
+
+-- Retorna somente o painel do placar
+ambPn2 :: Ambiente -> Panel ()
+ambPn2 (_, _, _, _, _, _, _, _, _, a, _, _) = a
+
+-- Retorna somente o item do menu que encerra a partida
+ambFch :: Ambiente -> MenuItem ()
+ambFch (_, _, _, _, _, _, _, _, _, _, a, _) = a
+
+-- Retorna somente a variável de som
+ambSom :: Ambiente -> Var Bool
+ambSom (_, _, _, _, _, _, _, _, _, _, _, a) = a
+
+
+-- Converte Estado para String
+strEstado :: Estado -> String
+strEstado e
+    | e == X  = strX
+    | e == O = strO
+    | otherwise   = strVazio
+
+
+-- Converte um tipo t para o tipo IO t
+toIO :: t -> IO t
+toIO t = do {return (t)}
