09f750b99629367ac15a4578e776c5159fec66d4
serginhofribeiro <serginhofribeiro@5c1608fb-56d2-20c3-ac56-ddccd30acc0e>
2010-05-31 01:57:39 +0000

    -Coloquei os comentários no início de cada documento. -Excluí o relatório porque ele tava com o padrão antigo.

--- trunk/Aleatorio.hs
+++ trunk/Aleatorio.hs
@@ -1,3 +1,32 @@
+------------------------------------------------------------------------------------------------
+--  Universidade de Pernambuco                                                                --
+--  Escola Politécnica de Pernambuco                                                          --
+--  Engenharia de Computação                                                                  --
+--  Disciplina: Linguagem de Programação Funcional                                            --
+--  Professora: Eliane Loiola                                                                 --
+------------------------------------------------------------------------------------------------
+--  Projeto: hs#                                                                              --
+------------------------------------------------------------------------------------------------
+--  Equipe:                                                                                   --
+--      Anderson de Oliveira Marques <andersonoliveiramarques@gmail.com>                      --
+--      Rodrigo Cesar Lira da Silva  <rodrigocliras@gmail.com>                                --
+--      Sergio Ferreira Ribeiro      <serginhofribeiro@gmail.com>                             --
+------------------------------------------------------------------------------------------------
+--  Objetivo:                                                                                 --
+--      Desenvolvimento do projeto referente ao segundo exercício escolar.                    --
+--                                                                                            --
+--  Descrição do Projeto:                                                                     --
+--      Este projeto é a implementação em Haskell do Jogo da Velha, com interface gráfica     --
+--      baseada na biblioteca hxHaskell. Pode-se jogar com dois jogadores humanos ou contra a --
+--      CPU. Foram utilizados recursos avançados do wxHaskell, visando um visual agradável,   --
+--      inclusive com a possibilidade de troca de "skins" e reprodução de áudios.             --
+------------------------------------------------------------------------------------------------
+--  Arquivo: Aleatorio.hs                                                                     --
+--      Módulo aleatório. Implementa funções de retorno aleatório.                            --
+------------------------------------------------------------------------------------------------
+--  Última Modificação: 30/05/2010                                                            --
+------------------------------------------------------------------------------------------------
+
 module Aleatorio (aleatorio, gerarDirecao) where
 
 import System.IO.Unsafe
